4a4567147550f02ed24cba9140263cc14424c8a9
IconElias254 <67762511+IconElias254@users.noreply.github.com>
2022-06-24 17:45:43 +0300

    Still experimenting

--- app.py
+++ app.py
@@ -15,34 +15,40 @@ def home():
     usage = 'Pass a properly encoded url parameter e.g. /https/www.google.com'
     return usage
 
+
 @app.route('/https/<url>')
-def root(url):    
+def root(url):
     url = 'https://' + url
     r = requests.get(url)
     rr = Response(response=r.content, status=r.status_code)
     rr.headers["Content-Type"] = r.headers['Content-Type']
     return rr
 
+
 @app.route('/g/<keyword>')
-def gkeyword(keyword):    
+def gkeyword(keyword):
     url = 'https://www.google.com/search'
-    payload = {'q':keyword, 'num':1, 'start':1, 'sourceid':'chrome', 'ie':'UTF-8', 'cr':'cr=countryUS'}
+    payload = {'q': keyword, 'num': 1, 'start': 1,
+               'sourceid': 'chrome', 'ie': 'UTF-8', 'cr': 'cr=countryUS'}
     r = requests.get(url, params=payload)
     rr = Response(response=r.content, status=r.status_code)
     rr.headers["Content-Type"] = r.headers['Content-Type']
     return rr
 
+
 @app.route('/r/<subreddit>/subscribers')
 def gsubreddit(subreddit):
     url = 'https://old.reddit.com/r/' + subreddit
-    xpath ="//span[@class='subscribers']/span[@class='number']/text()"
-    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    xpath = "//span[@class='subscribers']/span[@class='number']/text()"
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
     r = requests.get(url, headers=headers)
     tree = html.fromstring(r.content)
     subscribers = tree.xpath(xpath)
     rr = Response(response=subscribers, status=r.status_code)
     rr.headers["Content-Type"] = r.headers['Content-Type']
     return rr
+
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
